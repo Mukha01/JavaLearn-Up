82d2915f43bcbbcdcd0f774eba7efe35518f7f79
Aydar <aydar.mukhametzyanov.00@mail.ru>
2022-05-11 14:59:09 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        boolean isGreenLight = false;
+
+        int speedPlayer1 = 5;
+        int speedPlayer2 = 0;
+        int speedPlayer3 = 2;
+        int NumberOfEliminatedPlayers = 0;
+        {
+            if (isGreenLight == false) {
+                if (speedPlayer1 > 0) {
+                    NumberOfEliminatedPlayers += 1;
+                }
+            }
+            {
+                if (speedPlayer2 > 0)
+                    NumberOfEliminatedPlayers += 1;
+            }
+            {
+                if (speedPlayer3 > 0)
+                    NumberOfEliminatedPlayers += 1;
+            }
+
+            System.out.println("Количество выбывших игроков = " + NumberOfEliminatedPlayers);
+        }
+    }
+}
+
