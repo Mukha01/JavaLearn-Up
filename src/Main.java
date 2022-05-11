7fb5f72125902b350adf9650d60f59a58d99d53f
Aydar <aydar.mukhametzyanov.00@mail.ru>
2022-05-11 18:51:52 +0300

    Add a Good bye at the end

--- src/Main.java
+++ src/Main.java
@@ -25,6 +25,8 @@ public class Main {
 
             System.out.println("Количество выбывших игроков = " + NumberOfEliminatedPlayers);
         }
+        System.out.println("Good bye");
     }
 }
+
 
